23576e88b63dc3e8a1ecb7eb5e9309468035bd39
fr0zensn0w <snr.fr0zensn0w@gmail.com>
2020-05-28 10:12:55 -0400

    added typescript package

--- listeners/ready.js
+++ listeners/ready.js
@@ -1,4 +1,5 @@
 const { Listener } = require('discord-akairo');
+const { client } = require('../czbot')
 
 class ReadyListener extends Listener {
     constructor() {
@@ -9,7 +10,12 @@ class ReadyListener extends Listener {
     }
 
     exec() {
-        console.log('I\'m ready!');
+    let myGuild = client.guilds.cache.get('Put Server copy ID HERE');
+    let memberCount = myGuild.memberCount;
+    let memberCountChannel = myGuild.channels.cache.get('Put Channel Id Here');
+    memberCountChannel.setName('Member Count: ' + memberCount)
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
     }
 }
 
